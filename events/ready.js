29cc50c2f43d7e30a0ae1d1e90da7739f5ebd277
FrostieDev <martin.f.l@hotmail.dk>
2019-10-09 18:43:35 +0200

    Refactoring ready.js for modularity

--- events/ready.js
+++ events/ready.js
@@ -37,8 +37,8 @@ module.exports = bot => {
                                 console.log("This guild wants a new king"); //Test console.log
                                 guild = bot.guilds.get(`${getGuild.guildID}`); // Discord API call for guild with matching Guild ID
                                 sendToChannel = bot.channels.get(getGuild.channelID); // Discord API call for channel with matching Channel ID
-    
-                                    newKing(guild, getGuild, sendToChannel);
+                                
+                                newKingModule.newKing(guild,sendToChannel);
                                 
                             } else {
                                 cont1nue = false;
@@ -53,43 +53,5 @@ module.exports = bot => {
                 })
             }, 60 * 1000);
             //Interval check time end.
-    
-            let newKing = function (guild, newGuildInfo, messageChannel) {
-                var newBannedList = new BannedList();
-                var newGuildMember = new King(); // To put a database object into - kings to King
-                var validRole; // Discord role
-                var randomMember; // Take a random disc member from guild
-                // var listOfBannedStatus = []; // Array of banned status for the chosen guild.
-    
-                newKingModule.callGetBannedList(guild.id)
-                    .then(function (result) {
-                        //listOfBannedStatus = result;
-                        return newKingModule.randomMemberRecursion(result, newGuildInfo, guild);
-                    })
-                    .then(function (result) {
-                        randomMember = result;
-                        return newKingModule.callGetKing(guild.id, result.user["id"], guild);
-                    })
-                    .then(function (result) {
-                        newGuildMember = result;
-                        return newKingModule.removeMemberAsKing(newGuildInfo, messageChannel, guild);
-                    })
-                    .then(function (result) {
-                        validRole = result;
-                        return newKingModule.updateDiscRole(randomMember, validRole);
-                    })
-                    .then(function (result) {
-                        return newKingModule.callUpdateKingAmount(newGuildMember, newGuildInfo, messageChannel);
-                    })
-                    .then(function (result) {
-                        return newKingModule.callUpdateKing(newGuildMember, newGuildInfo);
-                    })
-                    .catch(function (err) {
-                        console.log("something went wrong: " + err);
-                    })
-    
-    
-    
-            };
     
 }
